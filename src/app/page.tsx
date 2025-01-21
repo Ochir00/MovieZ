c1eb3bf88b3e7e6d8a8b70e53580ce925451a8ff
Ochir00 <Mongolbucker@gmail.com>
2025-01-21 23:47:43 +0800

    3type cart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import { Popular } from "../components/cart";
+import { Popular, Top_rated, Upcoming } from "../components/cart";
 
 export default async function Home() {
 
   return (
     <div className="h-[100vh] w-[100vw]">
       <Popular />
+      <Upcoming />
+      <Top_rated />
     </div>
   );
 }
--- src/components/cart.tsx
+++ src/components/cart.tsx
@@ -17,7 +17,9 @@ export const Popular = async () => {
   return (
     <div className="w-[100vw] flex justify-center  " >
     <div className="flex flex-wrap justify-between w-[79.813rem]">
-
+    <h2>Popular</h2>
+    <p>see more!</p>
+    <div  className="flex flex-wrap justify-between w-[79.813rem]">
       {data.results.slice(0, 10).map((movie: MovieType) => {
         return (
           <div className="w-[14.375rem] h-[27.438rem] overflow-hidden rounded-[8px] ">
@@ -33,6 +35,87 @@ export const Popular = async () => {
           </div>
         );
       })}
+    </div>
+    </div>    
+    </div>
+  );
+};
+
+export const Upcoming = async () => {
+  // Fetch movie
+  const response = await fetch(
+    "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1",
+    {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const data = await response.json();
+
+  return (
+    <div className="w-[100vw] flex justify-center  " >
+    <div className="flex flex-wrap justify-between w-[79.813rem]">
+    <h2>Upcoming</h2>
+    <p>see more!</p>
+    <div  className="flex flex-wrap justify-between w-[79.813rem]">
+      {data.results.slice(0, 10).map((movie: MovieType) => {
+        return (
+          <div className="w-[14.375rem] h-[27.438rem] overflow-hidden rounded-[8px] ">
+            <img
+              src={`https://image.tmdb.org/t/p/original/${movie?.poster_path}`}
+              alt="..loading"
+              className="w-[14.358rem] h-[21.25rem]"
+            />
+            <div className="bg- bg-yellow-500 w-[14.358rem] h-[4.938rem] p-2">
+                <p>⭐{movie?.vote_average}/10</p>
+                <p>{movie?.original_title}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+    </div>    
+    </div>
+  );
+};
+
+export const Top_rated = async () => {
+  // Fetch movie
+  const response = await fetch(
+    "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+    {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const data = await response.json();
+
+  return (
+    <div className="w-[100vw] flex justify-center  " >
+    <div className="flex flex-wrap justify-between w-[79.813rem]">
+    <h2>Top Rated</h2>
+    <p>see more!</p>
+    <div  className="flex flex-wrap justify-between w-[79.813rem]">
+      {data.results.slice(0, 10).map((movie: MovieType) => {
+        return (
+          <div className="w-[14.375rem] h-[27.438rem] overflow-hidden rounded-[8px] overflow-hidden ">
+            <img
+              src={`https://image.tmdb.org/t/p/original/${movie?.poster_path}`}
+              alt="..loading"
+              className="w-[14.358rem] h-[21.25rem]"
+            />
+            <div className="bg- bg-yellow-500 w-[14.358rem] h-[4.938rem] p-2">
+                <p>⭐{movie?.vote_average}/10</p>
+                <p>{movie?.original_title}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
     </div>    
     </div>
   );
