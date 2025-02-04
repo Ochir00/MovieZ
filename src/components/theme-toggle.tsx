0404cdb93f8534659931f659ba89645c779d2b11
24LP2291 24LP2291 <24LP2291@G304CC0215.local>
2025-02-04 14:33:10 +0800

    za zasvar

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -11,7 +11,7 @@ import {
 } from "@/components/ui/dropdown-menu"
  
 export function ModeToggle() {
-  const {theme, setTheme} = useTheme("");
+  const {theme, setTheme} = useTheme();
   function webmode() {
     setTheme(theme == "dark" ? "light" : "dark" )
   }
