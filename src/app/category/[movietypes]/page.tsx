e3e968ae2277207d12bb0b557ef49ff39c75630d
Ochir00 <mongolbucker@gmail.com>
2025-02-05 13:56:39 +0800

    za see more bolson2

--- src/app/category/[movietypes]/page.tsx
+++ src/app/category/[movietypes]/page.tsx
@@ -1,45 +1,93 @@
 import { Card } from "@/components/ui/card";
-import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "@/components/ui/pagination";
-
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+import { TOKEN } from "../../../../util/constants";
+import Link from "next/link";
+import Image from "next/image";
+import { MovieType } from "../../../../util/MovieType";
 
 const moviepage = async ({
   params: { movietypes },
 }: {
   params: { movietypes: string };
 }) => {
+  // /movie/popular?language=en-US&page=1
 
-    return (
-        <Card className="w-[100vw] flex justify-center items-center">
-            <div className="w-[1280px]">
-                <h3></h3>
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/${movietypes}?language=en-US&page=1`,
+    {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  const data = await response.json();
 
+  return (
+    <Card className="w-[100vw] flex justify-center items-center">
+      <div className="w-[1280px]">
+        <h2 className="font-semibold text-[24px] mt-[50px]">{movietypes}</h2>
+        <div className="flex flex-wrap justify-between w-[79.813rem] mt-[20px]">
+          {data.results.slice(0, 20).map((movie: MovieType, index: number) => {
+            return (
+              <Card
+                key={index}
+                className="w-[14.375rem] h-[26.200rem] overflow-hidden rounded-[8px] mb-6"
+              >
+                <Link href={`/product-detail/${movie?.id}`}>
+                  <div>
+                    <Image
+                      src={`https://image.tmdb.org/t/p/original/${movie?.poster_path}`}
+                      alt="..loading"
+                      className="w-[14.358rem] h-[21.25rem]"
+                      width={1000}
+                      height={1000}
+                    />
+                    <div className="w-[14.358rem] h-[4.938rem] p-2">
+                      <p>⭐{movie?.vote_average}/10</p>
+                      <p className="font-semibold">{movie?.original_title}</p>
+                    </div>
+                  </div>
+                </Link>
+              </Card>
+            );
+          })}
+        </div>
 
-            <Pagination>
-              <PaginationContent>
-                <PaginationItem>
-                  <PaginationPrevious href="#" />
-                </PaginationItem>
-                <PaginationItem>
-                  <PaginationLink href="#">1</PaginationLink>
-                </PaginationItem>
-                <PaginationItem>
-                  <PaginationLink href="#" isActive>
-                    2
-                  </PaginationLink>
-                </PaginationItem>
-                <PaginationItem>
-                  <PaginationLink href="#">3</PaginationLink>
-                </PaginationItem>
-                <PaginationItem>
-                  <PaginationEllipsis />
-                </PaginationItem>
-                <PaginationItem>
-                  <PaginationNext href="#" />
-                </PaginationItem>
-              </PaginationContent>
-            </Pagination>
-            </div>
-        </Card>
-    )
-}
+        <Pagination className="mb-[50px]">
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious href="#" />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">1</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#" isActive>
+                2
+              </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">3</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationNext href="#" />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      </div>
+    </Card>
+  );
+};
 export default moviepage;
